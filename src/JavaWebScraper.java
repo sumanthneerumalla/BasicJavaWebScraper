b4c34e95aa81957ab0507cad4577e569f1a4e98e
sumanthneerumalla <sumanth42gold@gmail.com>
2016-11-08 14:20:40 -0500

    directly converting InputStream to byte array in order to get byte size of webpage

--- src/JavaWebScraper.java
+++ src/JavaWebScraper.java
@@ -1,4 +1,8 @@
+import sun.misc.IOUtils;
 import sun.misc.Queue;
+
+//import org.apache.commons.io.IOUtils;
+
 
 import java.util.*;
 import java.util.regex.Matcher;
@@ -8,18 +12,27 @@ import java.io.*;
 
 
 public class JavaWebScraper {
-    public static String getHTML(String urlToRead) throws Exception {
-        StringBuilder result = new StringBuilder();
+    public static int getURLSize(String urlToRead) throws Exception {
         URL url = new URL(urlToRead);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
+        InputStream pageStream = conn.getInputStream();
+
+//        byte[] pageInBytes = IOUtils.toByteArray(pageStream);
+
+        //alternative to IOUTILS
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int reads = pageStream.read();
+        while (reads != -1) {
+            baos.write(reads);
+            reads = pageStream.read();
         }
-        rd.close();
-        return result.toString();
+        byte[] pageInBytes = baos.toByteArray();
+
+        return pageInBytes.length;
+
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -34,6 +47,6 @@ public class JavaWebScraper {
 
 //        String webpage = Web.getWeb(startingUrl);
         System.out.println(startingUrl);
-        System.out.println(getHTML(startingUrl));
+        System.out.println(getURLSize(startingUrl));
     }
 }
