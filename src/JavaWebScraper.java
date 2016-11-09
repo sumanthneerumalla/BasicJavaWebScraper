a51429eaa481af99e9ac651fa6f126e097be5d08
sumanthneerumalla <sumanth42gold@gmail.com>
2016-11-09 16:47:07 -0500

    Tried several regular expressions to match anchor tags.  Using another regex to extract links.

--- src/JavaWebScraper.java
+++ src/JavaWebScraper.java
@@ -35,6 +35,51 @@ public class JavaWebScraper {
 
     }
 
+    public static Vector<String> parsePage(String urlToRead) throws Exception{
+        Vector<String> urlVector=new Vector<String>();
+//        String urlRegExp = "(https?:\\/\\/(?:www\\.|(?!www))[^\\s\\.]+\\.[^\\s]{2,}|www\\.[^\\s]+\\.[^\\s]{2,})";
+        String anchorRegExp = "<a\\s+(?:[^>]*?\\s+)?href=\"([^\"]*)\"";
+        String urlRegExp = "\"(.*)\"";
+//        String urlRegExp = "(<a.*?>.*?</a>)";
+        Pattern anchorPattern = Pattern.compile(anchorRegExp);
+        Matcher anchorMatcher = anchorPattern.matcher(getHTML(urlToRead));
+
+        Pattern urlPattern = Pattern.compile(urlRegExp);
+
+
+        while(anchorMatcher.find()){
+
+            String nextAnchor = anchorMatcher.group();
+            urlVector.add(nextAnchor);
+            System.out.println(nextAnchor);
+
+            Matcher urlMatcher = urlPattern.matcher(nextAnchor);
+            if (urlMatcher.find())
+            {
+                System.out.println(urlMatcher.group(1));
+            }
+        }
+        System.out.println(urlVector.size());
+
+
+        return urlVector;
+    }
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+
     public static void main(String[] args) throws Exception {
         String startingUrl = args[0];
 
@@ -48,5 +93,7 @@ public class JavaWebScraper {
 //        String webpage = Web.getWeb(startingUrl);
         System.out.println(startingUrl);
         System.out.println(getURLSize(startingUrl));
+        System.out.println(getHTML(startingUrl));
+        parsePage(startingUrl);
     }
 }
